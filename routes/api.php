81e309c7ff0e396f90d65a90bafbf13a2c87b2d7
drobash665 <AD10122004@mail.ru>
2026-02-10 11:45:30 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BrandsControllerApi;
 use App\Http\Controllers\FragranceControllerApi;
 use App\Http\Controllers\OrderControllerApi;
 use App\Http\Controllers\UserControllerApi;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/brand', [BrandsControllerApi::class, 'index']);
+Route::middleware('auth:sanctum')->get('/brand', [BrandsControllerApi::class, 'index']);
 Route::get('/brand/{id}', [BrandsControllerApi::class, 'show']);
 Route::get('/order', [OrderControllerApi::class, 'index']);
 Route::get('/order/{id}', [OrderControllerApi::class, 'show']);
 Route::get('/fragrance', [FragranceControllerApi::class, 'index']);
 Route::get('/fragrance/{id}', [FragranceControllerApi::class, 'show']);
-Route::get('/user', [UserControllerApi::class, 'index']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 Route::get('/user/{id}', [UserControllerApi::class, 'show']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/brand', [BrandsControllerApi::class, 'index']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    route::get('/logout', [AuthController::class, 'logout']);
+});
